bc8840e6d5dec82a5d6aa5e5f45306306449900e
masoud <masood.hsn82@gmail.com>
2024-12-04 12:33:28 +0330

    kaypad
    program kaypad that return   0x(row)(col)   that row and col are a nible or hex

--- Code/code.c
+++ Code/code.c
@@ -3,31 +3,25 @@
 
 #define E PORTC.4
 #define RS PORTC.5
-#define E_delay 1
 
+
+void send_lcd( char bits){
+  PORTC=PORTC & 0xF0 | bits;  
+  E=0;
+  delay_ms(1); 
+  E=1;
+}
 
 void send_command(char command){
-  RS=0;    
-  PORTC=PORTC & 0xF0 | (( command & 0xF0 ) >> 4);  
-  E=0;
-  delay_ms(E_delay); 
-  E=1;                      
-  PORTC=PORTC & 0xF0 | (( command & 0x0F ));  
-  E=0;
-  delay_ms(E_delay); 
-  E=1;
+  RS=0;     
+  send_lcd (( command & 0xF0 ) >> 4);
+  send_lcd( command & 0x0F );                    
   RS=1; 
 }
 
 void char_out(char out){
-  PORTC=PORTC & 0xF0 | (( out & 0xF0 ) >> 4);  
-  E=0;
-  delay_ms(E_delay); 
-  E=1;                      
-  PORTC=PORTC & 0xF0 | (( out & 0x0F ));  
-  E=0;
-  delay_ms(E_delay); 
-  E=1;
+  send_lcd(( out & 0xF0 ) >> 4);
+  send_lcd( out & 0x0F );
 }
 
 void lcd_init(void) {
@@ -77,20 +71,48 @@ clear_display();
 }
 
 
+char row(char col){
+      char out;
+      PORTA.3=1;
+      if(( PINA & 0x07) != col) out = 0xA0;      
+      PORTA.3=0;
+      PORTA.4=1;
+      if(( PINA & 0x07) != col) out = 0xB0;
+      PORTA.4=0;
+      PORTA.5=1;
+      if(( PINA & 0x07) != col) out = 0xC0;
+      PORTA.5=0;
+      PORTA.6=1;
+      if(( PINA & 0x07) != col) out = 0xD0;
+      PORTA.6=0;    
+      return out;
+}
+
+char key(){
+  if ( (PINA & 0x07) == 0x07  ) return  0x00 ; 
+  if ((PINA & 0x07) == 0x03  )   return 0x01 | row(0x03);
+  if ((PINA & 0x07) == 0x05  )   return 0x02 | row(0x05 );
+  if ((PINA & 0x07) == 0x06  )   return 0x03 | row(0x06);
+}
+
+
 void main(void)
 {
 
-  DDRC=0xFF;
+  DDRC=0xFF;    
+  DDRA=0xF8;
   //init LCD
   lcd_init();              
+  //init key pad
+  PORTA=0x07;
      
   menu(0);
-  
-                                                       
+                                           
 
   while(1){
    display_shift();
-   delay_ms(25);
+   delay_ms(25);   
+   key();
   }    
   
 }
