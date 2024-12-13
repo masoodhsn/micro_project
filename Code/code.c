2fec819585530597ea79e3c1496ff6cf40e4c393
masoud <masood.hsn82@gmail.com>
2024-12-13 11:33:50 +0330

    finishing secound key (create bug in secound and threard key)

--- Code/code.c
+++ Code/code.c
@@ -80,20 +80,34 @@ void print(char* out, unsigned char it){
 
 void menu(char page){
 clear_display();
+  PORTD=page;
  switch (page){
-  case 0: 
-  print("1)Initialization 3)Presents    5)USART\n2)Search         4)Temperature 6)Traffic",0x00); 
+  case 0:      
+  print("1)Initialization 3)Presents    5)USART\n2)Search         4)Temperature 6)Traffic",0x00);
   break;
   case 11:
   print("Attendance\nInitialization",0x00);
   break;         
-  case 1:  
+  case 1:
+  case 2:  
     print("Exit:* ,Delete:#\n",0x00);
     print(code,cursor);
   break;
-  case 12: 
+  case 12:   
+  case 22:
   print("Enter:*,Delete:#\n",0x00);  
   print(code,cursor);
+  break;    
+  case 21:
+  print("Students\nManagment",0x00);    
+  break;                               
+  case 23:             
+  print("Present",0x00);
+  print(show,0x40);
+  break;
+  case 24:       
+  print("Absent",0x00);    
+  print(code,0x40);
   break;
   case 31: 
   print("Present Students\nExit:* Number:",0x00);      
@@ -183,23 +197,59 @@ unsigned int text_size(char * in){
     return out;  
 }
 
+char check_student(){
+  char*it=student;      
+  char number=0 ;   
+  char i=0; 
+  show[11]=0x00;
+  show[2]=')';
+  
+  while(*it){  
+     i=0;
+     for ( ;i<9;i++){
+        if(*(it+i) != code[i]) break;
+        if (code[i]==0x00){           
+          show[0]=number/10;
+          show[1]=number%10; 
+          return 0x01;
+        }  
+        show[i+3]=code[i];
+     }  
+     number +=1;     
+     it=student+number*8;
+  }        
+  return 0x00;    
+}
+
 
 void display(char go){ 
+
+    ///////////////////////// timer
+
    if (page==0 && time%25==0 ){
     display_shift(time/25);  
     if(time==900) time=0;
-  }       
+  }                                       
+  
   else if (page ==11 && time == 100 ){
     page=1;
     menu(1);
     time=0;
+  }            
+  
+  else if((page == 21 || page == 23 || page == 24) && time == 200){
+   page=2;
+    menu(2);
+    time=0; 
   }
+  
   else if (page ==31 && time == 200){
     page =3;
     time=0;
     return;
   }   
-  
+    
+    ///////// showing number in shift mode
    else if( page==3 && time%25==0 && go == 0x00){   
         char number=text_size(student)/8;   
         if (time==((number-1)/6+1)*900) time=0;  
@@ -210,23 +260,47 @@ void display(char go){
         }          
         display_shift((time%900)/25);       
   }   
-  
-  
-  if(page == 0 && go ==1){
+   ///////////////////////////////////////////////end timer
+          
+   ///////////////////////////////////////////click on page 0     
+    if(page == 0 && go ==1){
     time =0;
     page=11;
     menu(11); 
-  }
+  }   
+  
+   else if (page == 0 && go == 0x02){               
+    time =0;
+    page=21;
+    menu(21);   
+  }                  
+  
+   else if (page == 0 && go == 0x03){  
+    char number=(text_size(student)/8)/10+0x30;   
+    *((&number)+1)=(text_size(student)/8)%10+0x30;
+    *((&number)+2)=0x00;                  
+    time =0;
+    page=31;
+    menu(31);   
+    print(&number,cursor);
+  }     
+    ////////////////////////////////// end click on page 0
+  
+  
+  ///////////////////////////////////////////click on page 1  and 2                                                                                                       
+     
+  ////// press any key to skip info page    1
   else if(page == 11 && go != 0){
     page =1;
     menu(1);
-  } 
+  }     
+  //// exit    1
   else if(page ==1 && go == 0xD1){                    
     char i=0x00; 
     for(;i<0x08;i++) code[i]=0x00;
    go_menu();     
   }    
-  
+  //// enter    1
   else if (page==12 && go == 0xD1){ 
     unsigned int temp=text_size(student);   
     char i=0x00; 
@@ -237,22 +311,59 @@ void display(char go){
     menu(1);
     page=1;
   }
-  
+   //// Delete     1
   else if((page ==1 || page == 12) && go == 0xD3){
    if (text_size(code))
      code[text_size(code)-0x01]=0x00;
       menu(1);
       page =1;
+  }        
+  
+   ////// press any key to skip info page    2
+  else if((page == 21 || page ==23 || page ==24) && go != 0){
+    page =2;
+    menu(2);
+  }     
+  //// exit     2
+  else if(page ==2 && go == 0xD1){                    
+    char i=0x00; 
+    for(;i<0x08;i++) code[i]=0x00;
+   go_menu();     
+  }    
+  //// enter    2
+  else if (page==22 && go == 0xD1){    
+    char i=0;  
+    time=0;
+    if(check_student()){
+      page=23;
+      menu(23);
+    }  
+    else{
+      page=24;
+      menu(24);   
+  }               
+  for(;i<8;i++) code[i]=0x00;
+  }
+   //// Delete   2
+  else if((page ==2 || page == 22) && go == 0xD3){
+   if (text_size(code))
+     code[text_size(code)-0x01]=0x00;
+      menu(2);
+      page =2;
   } 
+  
    
-  else if((page ==1 || page==12) && go!=0xD1 && go!= 0xD3 && go != 0x00){   
+  /// input numbers
+  else if(((page ==1 || page==12) || (page ==2 || page==22)) && go!=0xD1 && go!= 0xD3 && go != 0x00){   
+    /// buzzer error
     if (text_size(code) == 0x08){
       print("Error",cursor+2);    
       PORTC.6=1;
       delay_ms(25);
       PORTC.6=0;
       return ;
-    }   
+    }        
+    //number
     if(go == 0xD2){  
      print("0",cursor);
      code[text_size(code)]=0x30;  
@@ -264,28 +375,33 @@ void display(char go){
        *(p+1)=0x00;
        print(p,cursor);  
        code[text_size(code)]=num;  
-     }    
-     if (text_size(code) == 0x08){
-      menu(12);
-      page=12;    
-     }
-  }
+     }     
     
-  
+      /// go to enter mode 
+     if (text_size(code) == 0x08){ 
+      if (page ==1){
+      menu(12);
+      page=12;  
+      }
+      else{ 
+       menu(22);
+      page=22;
+      } 
+     } 
+     }  
+          
+    
+  ////////////////////////////////// end click on page 1    and   2
+                                                                                                     
 
-  else if (page == 0 && go == 0x03){  
-    char number=text_size(student)/8+0x30;  
-    *((&number)+1)=0x00;                 
-    time =0;
-    page=31;
-    menu(31);   
-    print(&number,cursor);
-  } 
+  
+                                                        
+  /////////////////////////////// click on page 3
    
-  else if ((page == 31 || page ==3) && go== 0xD1){
+  else if ((page == 31 || page ==3) && go== 0xD1){ 
     go_menu();
-  }
-     
+  }                                                                   
+  //////////////////////////end click page 3
 
   time=time+1; 
 }
@@ -295,7 +411,6 @@ void main(void)
   DDRC=0xFF;    
   DDRA=0xF8;
   DDRD=0xFF;  
-  PORTD=0xFF;
   //init LCD
   lcd_init();              
   //init key pad
